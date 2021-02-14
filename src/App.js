6ef5c4a596d91a320f7b834c476af7113bac7596
António Abrantes <antonio.abrantes@amsainfor.com>
2021-02-14 03:18:28 +0000

    b

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tony das couves || Lelito || 2222 || Edit || with NGINX <code>src/App.js</code> and save to reload.
+          Tony das couves || with NGINX <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
